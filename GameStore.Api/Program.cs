f63cd6cc4c33559ce52bf6ac0d7cb713a58c222a
Thilina Prasad <thilina1091@gmail.com>
2024-07-12 21:53:04 +0530

    updated

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -41,14 +41,15 @@ var app = builder.Build();
 
 app.MapGet("/games", () => games);
 
-app.MapGet("/games/{id}",(int id) => {
-Game? game = games.Find(game => game.Id == id);
-
-if (game is null)
+app.MapGet("/games/{id}", (int id) =>
 {
-    return Results.NotFound();
-}
-return Results.Ok(game);
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(game);
 })
 .WithName(GetGameEndpointName);
 
@@ -57,7 +58,38 @@ app.MapPost("/games", (Game game) =>
     game.Id = games.Max(game => game.Id) + 1;
     games.Add(game);
 
-    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id},game);
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
+
+app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+{
+    Game? existingGame = games.Find(game => game.Id == id);
+
+    if (existingGame is null)
+    {
+        return Results.NotFound();
+    }
+    existingGame.Name = updatedGame.Name;
+    existingGame.Genre = updatedGame.Genre;
+    existingGame.Price = updatedGame.Price;
+    existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.ImageUri = updatedGame.ImageUri;
+
+    return Results.NoContent();
+});
+
+
+app.MapDelete("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is not null)
+    {
+        games.Remove(game);
+    }
+
+
+    return Results.NoContent();
 });
 
 app.Run();
--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5dad37f98939655cc7f211a919897531eced99e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b68d2172ff7fce40532d97cce4cc8b8648a5aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
